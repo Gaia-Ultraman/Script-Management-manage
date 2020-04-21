9c0df400d02b7651b806082efb532d895dd8d893
liqiang <1281991712@qq.com>
2020-04-21 22:56:52 +0800

    上线下线通知，

--- src/pages/pic/cards/index.js
+++ src/pages/pic/cards/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Checkbox, Card, Pagination,Modal } from "antd"
- import {ReloadOutlined} from "@ant-design/icons"
+import { Checkbox, Card, Pagination, Modal, message } from "antd"
+import { ReloadOutlined } from "@ant-design/icons"
 import styles from "./index.less"
 const { Meta } = Card
 
@@ -9,10 +9,10 @@ export default class MyCard extends React.Component {
     state = {
         page: 1,
         pageSize: 10,
-        visible:false,
+        visible: false,
         //放大的视图
-        url:"",
-        name:""
+        url: "",
+        name: ""
     }
 
     handleCheck = (e, id) => {
@@ -30,56 +30,69 @@ export default class MyCard extends React.Component {
         this.setState({
             page, pageSize
         })
-        this.sendData({type:"getPic",data:devices.slice((page-1) * pageSize, page * pageSize).map(v=>v.id)})
+        this.sendData({ type: "getPic", data: devices.slice((page - 1) * pageSize, page * pageSize).map(v => v.id) })
     }
-    sendData=(data)=>{
-        const {sendFunc}=this.props
+    sendData = (data) => {
+        const { sendFunc } = this.props
         sendFunc(data)
     }
 
-    componentDidMount(){
-        const { devices } = this.props
-        const { page, pageSize } = this.state
-        this.sendData({type:"getPic",data:devices.slice(0, page * pageSize).map(v=>v.id)})
+    handleAmplification=(value)=>{
+        message.error("没有可以预览的截图！")
+        if(value.url){
+            this.setState({ visible: true, url: value.url, name: value.name }) 
+        }
+        
+    }
+  
+    componentWillReceiveProps(nextProps){
+        const { devices } = nextProps;
+        const { page, pageSize} = this.state
+        if(devices.length){
+            this.sendData({data:{codeType:"device",cmd:"getSnapshot"}},{group:"phone",id:devices.slice(0, page * pageSize).map(v=>v.id)})
+        }
     }
 
     render() {
         const { devices } = this.props
-        const { page, pageSize,url,name ,visible} = this.state
+        const { page, pageSize, url, name, visible } = this.state
         return (
             <div className={styles.cardContainer}>
                 <div className={styles.cards}>
                     {
-                        devices.slice((page - 1) * pageSize, page * pageSize).map(value => {
+                        devices.map(value => {
                             return <Card key={value.id}
-                                style={{ width: 150, marginTop: "0.3rem", marginLeft: "0.4rem", marginRight: "0.4rem" }}
+                                style={{ width: 165, marginTop: "0.3rem", marginLeft: "0.4rem", marginRight: "0.4rem" }}
                                 cover={
-                                    <div onClick={()=>{debugger; this.setState({visible:true,url:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",name:value.name})}}>
-                                        <ReloadOutlined  className={styles.icon}/>
-                                        <img
-                                            style={{ width: 150, height: 200 }}
+                                    <div onClick={() => { this.handleAmplification(value) }}>
+                                        <ReloadOutlined className={styles.icon} />
+                                        {value.img ? <img
+                                            style={{ width: 165, height: 200 }}
                                             alt="屏幕截图"
-                                            src={value.img?value.img:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"}
-                                        />
+                                            src={value.img}
+                                        /> : <div style={{ width: 165, height: 200, display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                                未获取截图
+                                            </div>
+                                        }
                                     </div>
                                 }>
                                 <Meta
                                     title={<Checkbox checked={value.checked} onClick={(e) => { this.handleCheck(e, value.id) }}>{value.name}</Checkbox>}
-                                    description={value.des?value.des:"This is the description"}
+                                    description={value.des ? value.des : "This is the description"}
                                 />
                             </Card>
                         })
                     }
                 </div>
-                <Pagination defaultCurrent={1} total={devices.length} onChange={this.handlePage} />
+                {/* <Pagination defaultCurrent={1} total={devices.length} onChange={this.handlePage} /> */}
                 <Modal
-                title={name}
-                visible={visible}
-                onCancel={()=>{this.setState({visible:false})}}
-                footer={null}
-            >
-                <img src={url} style={{width:"100%"}}></img>
-            </Modal>
+                    title={name}
+                    visible={visible}
+                    onCancel={() => { this.setState({ visible: false }) }}
+                    footer={null}
+                >
+                    <img src={url} style={{ width: "100%" }}></img>
+                </Modal>
             </div>
         );
     }
--- src/pages/pic/index.js
+++ src/pages/pic/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Checkbox, Button,  message, Modal, Input, Select } from "antd"
+import { Checkbox, Button, message, Modal, Input, Select, Slider } from "antd"
 import Cards from "./cards"
 import Bottom from "./bottom"
 import { PlusOutlined } from '@ant-design/icons';
@@ -13,10 +13,11 @@ export default class App extends React.Component {
     ws = null
     state = {
         tempId: new Date().getTime(),
+        //服务端连接状态
         hasConnect: false,
         groups: getGroup(),
         //所有的设备
-        allDevices: [{ id: "1025", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }, { id: "1024", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }, { id: "1023", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }, { id: "1022", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }, { id: "1021", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }, { id: "1020", name: "iphone", url: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png", lastLog: "开机" }],
+        allDevices: [],
         //需要展示的设备
         showDevices: [],
         //目前选中的group
@@ -32,12 +33,17 @@ export default class App extends React.Component {
         addVisible: false,
         addName: "",
         type: 2,
-        reg: ""
+        reg: "",
+
+
+        //服务端连接
+        url: "",
+
     };
     componentDidMount() {
         document.title = "ID:" + this.state.tempId
-        // this.connect()
-        this.setState({showDevices:this.state.allDevices})
+        let url = localStorage.getItem("url")
+        this.setState({ url })
     }
 
     componentWillUnmount() {
@@ -46,15 +52,54 @@ export default class App extends React.Component {
 
     //连接SOCKET
     connect = () => {
-        const { tempId } = this.state
-        this.ws = new WebSocket(`ws://localhost:8080/manager?id=${tempId}`)
+        const { tempId, url } = this.state
+        if (!url) {
+            message.error("连接不能为空!")
+            return
+        }
+        this.ws = new WebSocket(`${url}/manager?id=${tempId}`)
         this.ws.addEventListener('message', (event) => {
-            console.log('Message from server ', event.data, this.state);
+            let result = {}
+            try {
+                result = JSON.parse(event.data)
+            } catch (err) {
+                console.log("消息返回解析错误:", err)
+                return
+            }
+            const {currentGroup,groups,showDevices,allDevices}=this.state
 
+            //获得所有设备，更新设备状态
+            if(result.from && result.from.group =="server"){
+                if(result.data && result.data.cmd =="getAllOnline"){
+                    this.setState({ showDevices: result.data.retMsg,allDevices:result.data.retMsg, currentGroup: "全部", checked: false })
+                }else if(result.data && result.data.cmd =="online"){
+                    message.success(`${result.data.retMsg.name} 上线`)
+                    allDevices.push(result.data.retMsg)
+                    this.setState({
+                        allDevices:JSON.parse(JSON.stringify(allDevices))
+                    },()=>{
+                        this.handleGroup(groups.filter(v=>v.name==currentGroup)[0])
+                    })
+                    
+                }else if(result.data && result.data.cmd =="offline"){
+                    message.error(`${result.data.retMsg.name} 下线`)
+                    this.setState({
+                        allDevices:JSON.parse(JSON.stringify(allDevices.filter(v=>v.id!=result.data.retMsg.id)))
+                    },()=>{
+                        this.handleGroup(groups.filter(v=>v.name==currentGroup)[0])
+                    })
+                }
+            }
+            //获取图片
+            else if( result.from && result.from.group =="phone"){
+                
+            }
+
+            console.log('Message from server ', event.data, this.state);
         });
         this.ws.addEventListener('error', (event) => {
             console.log('Error', event);
-            message.error("服务器连接失败!请手动连接!")
+            message.error("服务器连接失败!")
         });
 
         this.ws.addEventListener('open', (event) => {
@@ -62,6 +107,7 @@ export default class App extends React.Component {
             this.setState({
                 hasConnect: true,
             })
+            this.getAllDevices()
             message.success("服务器连接成功!")
         });
 
@@ -70,6 +116,9 @@ export default class App extends React.Component {
             message.info("连接已关闭!")
             this.setState({
                 hasConnect: false,
+                showDevices:[],
+                allDevices:[],
+                checked: false
             })
         });
 
@@ -77,22 +126,22 @@ export default class App extends React.Component {
 
     //筛选
     handleGroup = (value) => {
-        console.log("value",value)
+        console.log("value", value)
         const { allDevices } = this.state
         let showDevices = [];
         //去掉checked属性
-        let cloneAllDevices=allDevices.map(v=>{
-            let temp=JSON.parse(JSON.stringify(v))
+        let cloneAllDevices = allDevices && allDevices.map(v => {
+            let temp = JSON.parse(JSON.stringify(v))
             delete temp.checked;
             return temp
         });
 
-        if(value.name=="全部"){
-            this.setState({ showDevices:allDevices, currentGroup: value.name, checked: false })
+        if (value.name == "全部") {
+            this.setState({ showDevices: allDevices, currentGroup: value.name, checked: false })
             return
         }
-        
-        
+
+
         //filter 返回的子元素是引用类型时，需要深拷贝一下数组，不然会影响原数据
         if (value.type == 1) {
             let reg = new RegExp(value.reg)
@@ -135,22 +184,22 @@ export default class App extends React.Component {
 
     //==========================添加模态框==========================
     handleOk_add = () => {
-        const { addName, type, reg ,showDevices} = this.state
+        const { addName, type, reg, showDevices } = this.state
         console.log("OK:", addName, type, reg)
-        let item={name:addName,type}
-        if(type==1){
-            item.reg=reg
-        }else{
-            item.data=showDevices.filter(v=>v.checked).map(v=>v.id)
+        let item = { name: addName, type }
+        if (type == 1) {
+            item.reg = reg
+        } else {
+            item.data = showDevices.filter(v => v.checked).map(v => v.id)
         }
-        let result=setGroup(item)
-        if(!result){
+        let result = setGroup(item)
+        if (!result) {
             message.error("添加失败！")
             return
         }
         this.setState({
-            groups:result,
-            addVisible:false
+            groups: result,
+            addVisible: false
         })
         this.handleGroup(item)
     }
@@ -178,42 +227,91 @@ export default class App extends React.Component {
     }
 
     //处理设备被check
-    handleCardCheck=(showDevices)=>{
-        this.setState({showDevices})
+    handleCardCheck = (showDevices) => {
+        this.setState({ showDevices })
     }
 
     //底部回调的值
-    handleBottomObj=(obj)=>{
-        console.log("BottomCB",obj)
+    handleBottomObj = (obj) => {
+        console.log("BottomCB", obj)
     }
 
-    //调用websocket发送消息
-    sendMessage=(data)=>{
-        this.ws&&this.ws.send(JSON.stringify(data))
+
+    //调用websocket发送消息 
+    sendMessage = (data, dis) => {
+        const { hasConnect, tempId } = this.state
+        if (!hasConnect) {
+            message.error("未连接，执行失败！")
+            return
+        }
+        let from = { group: "console", id: tempId }
+        this.ws && this.ws.send(JSON.stringify({
+            from,
+            data,
+            dis
+        }))
+    }
+
+
+    //操作连接地址Input
+    handleInputUrl = (e) => {
+        this.setState({ url: e.target.value })
+        //缓存
+        localStorage.setItem("url", e.target.value)
+    }
+
+    //  连接/断开按钮
+    handleConnect = () => {
+        const { hasConnect } = this.state
+        if (hasConnect) {
+            this.ws && this.ws.close()
+        } else {
+            this.connect()
+        }
+    }
+
+    //获取所有设备
+    getAllDevices = () => {
+        this.sendMessage({ cmd: "getAllOnline" },{ group: "server" })
+    }
+
+    //滑动条
+    handleSlider = (value) => {
+        this.sendMessage({ codeType: "device", cmd: "setScreenBrightness", brightness: value }, { group: "phone", id: "all" })
     }
 
     render() {
-        const { groups, allDevices, currentGroup, visible, checked, addVisible, type,showDevices } = this.state
+        const { groups, allDevices, currentGroup, visible, checked, addVisible, type, showDevices, hasConnect, url } = this.state
         return <div className={styles.container}>
-            <div>
-                <Button type="primary" danger onClick={this.handleModal_delete} className={styles.notice}>删除分组</Button>
-                <Checkbox checked={checked} onClick={this.handleCheck}>全选</Checkbox>
+            <div className={styles.header}>
+                <div>
+                    <Button type="primary" danger onClick={this.handleModal_delete} className={styles.notice}>删除分组</Button>
+                    <Checkbox checked={checked} onClick={this.handleCheck}>全选</Checkbox>
+                    <span style={{ paddingLeft:"30px"}}>当前分组数量:<span style={{ color: "green",fontSize:20 }}>{showDevices.length}</span></span>
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>调节屏幕亮度:<Slider className={styles.slider} defaultValue={30} max={100} min={0} onAfterChange={this.handleSlider} /></div>
+                <div className={styles.status}>
+                    {hasConnect ? <div style={{ color: "green" }}>已连接</div> : <div style={{ color: "red" }}>未连接</div>}
+                    <Input value={url} placeholder={"服务器地址"} onChange={this.handleInputUrl} />
+                    <Button onClick={this.handleConnect}>{hasConnect ? "断开" : "连接"}</Button>
+                </div>
             </div>
+
             <div className={styles.content}>
 
                 <div className={styles.groupList}>
                     {groups.map((value, i) => {
-                        return <Button type={currentGroup == value.name ? "primary" : null} key={i} onClick={() => { this.handleGroup( value) }} >{value.name}</Button>
+                        return <Button type={currentGroup == value.name ? "primary" : null} key={i} onClick={() => { this.handleGroup(value) }} >{value.name}</Button>
                     })}
                     <Button onClick={this.handleModal_add}><PlusOutlined style={{ fontSize: 45, color: "rgba(0,0,0,0.3)" }} /></Button>
                 </div>
 
-                
-                <Cards devices={showDevices} onChecked={this.handleCardCheck} sendFunc={this.sendMessage}/>
-                
+
+                <Cards devices={showDevices} onChecked={this.handleCardCheck} sendFunc={this.sendMessage} />
+
             </div>
 
-            <Bottom callBack={this.handleBottomObj} sendFunc={this.sendMessage}/>
+            <Bottom callBack={this.handleBottomObj} sendFunc={this.sendMessage} />
 
             <Modal
                 title="删除分组"
